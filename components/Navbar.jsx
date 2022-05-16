e888b3fff04f2e6c16cb37d3758ccd61637f5ca8
Joshua Burger <joshberger6969@outlook.com>
2022-05-16 15:48:25 -0400

    Add className to style navbar links and logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,15 +4,15 @@ import styles from '../styles/Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
-            <h6>E-commerce</h6>
-            <ul>
-                <li>
+            <h6 className={styles.logo}>E-commerce</h6>
+            <ul className={styles.links}>
+                <li className={styles.navlink}>
                     <Link href="/">Home</Link>
                 </li>
-                <li>
+                <li className={styles.navlink}>
                     <Link href="/shop">Shop</Link>
                 </li>
-                <li>
+                <li className={styles.navlink}>
                     <Link href="/cart">Cart</Link>
                 </li>
             </ul>
